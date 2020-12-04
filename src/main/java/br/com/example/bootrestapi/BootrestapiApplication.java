31d75ed8b4684cc06906b37313b8ed48b1742f0c
jwiltonalves <75493292+jwiltonalves@users.noreply.github.com>
2020-12-04 13:28:02 -0300

    commit inicial
    Estrutura padrão do projeto gerada no initializr (https://start.spring.io/)
    
    https://start.spring.io/#!type=maven-project&language=java&platformVersion=2.3.6.RELEASE&packaging=jar&jvmVersion=11&groupId=br.com.example&artifactId=bootrestapi&name=bootrestapi&description=&packageName=br.com.example.bootrestapi&dependencies=lombok,devtools,web,data-jpa,h2

---
+++ src/main/java/br/com/example/bootrestapi/BootrestapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.example.bootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootrestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/example/bootrestapi/BootrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.bootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
